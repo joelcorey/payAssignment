8296557b4c1208b2e7670072c409be3397b712f4
Joel Corey <joelcorey@fastmail.com>
2018-03-01 07:37:53 -0600

    Working javascript added

--- justdoit.js
+++ justdoit.js
@@ -3,7 +3,7 @@
 function doit() {
     let hours = document.getElementById("hoursFormInput").value;
     let pay = document.getElementById("payrateFormInput").value;
-    console.log(hours * pay);
+    console.log(hours * pay):
 }
 
     
