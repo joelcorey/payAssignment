8c2f68508152879cb1af4855ef4e63226491db61
Joel Corey <joelcorey@fastmail.com>
2018-03-01 08:58:17 -0600

    Fixed JavaScript displaying in p element

--- justdoit.js
+++ justdoit.js
@@ -3,7 +3,16 @@
 function doit() {
     let hours = document.getElementById("hoursFormInput").value;
     let pay = document.getElementById("payrateFormInput").value;
-    console.log(hours * pay):
+    //console.log(hours * pay);
+    //let calculate = hours * pay;
+    //document.getElementById("card-text").value = calculate;
+
+    if (hours > 40) {
+        hours = hours * 1.5;
+        console.log(hours);
+    }
+
+    document.getElementById("display-money").innerHTML = hours * pay;
 }
 
     
